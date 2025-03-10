9bb52fd683769cb1ecf5ca89def10e101cc4cd19
Dean Denaki <sonicmailfaster@gmail.com>
2025-03-10 20:40:55 +0700

    rebase & rename pages

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,19 @@ Route::get('/', function () {
 });
 
 Route::get('products', function () {
-    return view('pages.product');
+    return view('pages.products.index');
 });
 
 Route::get('customers/lists', function () {
-    return view('pages.customer.list');
+    return view('pages.customer.list.index');
 });
 
 Route::get('customers/contacts', function () {
-    return view('pages.customer.contact');
+    return view('pages.customer.contact.index');
 });
 
 Route::get('customers/services', function () {
-    return view('pages.customer.services');
+    return view('pages.customer.service.index');
 });
 
 Route::get('login', function () {
