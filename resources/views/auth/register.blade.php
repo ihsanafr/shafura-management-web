2b4442dcb29a467f72927000fdb2c6ecb8a9747a
Dean Denaki <sonicmailfaster@gmail.com>
2025-03-11 21:37:42 +0700

    implement login and register revision

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,67 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login Page</title>
-    <script src="https://kit.fontawesome.com/2b9cba7764.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-</head>
-<body class="bg-light">
+@extends('layouts.auth')
 
-<section class="d-flex justify-content-center align-items-center min-vh-100">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-                <div class="card border border-light-subtle rounded-3 shadow-sm">
-                    <div class="card-body p-3 p-md-4 p-xl-5">
-                        <div class="text-center mb-3">
-                            <a href="#!">
-                                <p>Shafura Digital Indonesia</p>
-                            </a>
-                        </div>
-                        <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Enter your details to register</h2>
-                        <form action="#!">
-                            <div class="row gy-2 overflow-hidden">
-                                <div class="col-12">
-                                    <div class="form-floating mb-3">
-                                        <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last Name" required>
-                                        <label for="lastName" class="form-label">username</label>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-floating mb-3">
-                                        <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
-                                        <label for="email" class="form-label">Email</label>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="form-floating mb-3">
-                                        <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
-                                        <label for="password" class="form-label">Password</label>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="d-grid my-3">
-                                        <button class="btn btn-primary btn-lg" type="submit">Register</button>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <p class="m-0 text-secondary text-center">Already have an account? <a href="#!" class="link-primary text-decoration-none">Login</a></p>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+
+@section('main')
+    <div class="text-center mb-3">
+        <a href="#!">
+            <p>Shafura Digital Indonesia</p>
+        </a>
+    </div>
+    <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Enter your details to register</h2>
+    <form method="POST" action="{{ route('register.store') }}">
+        @csrf
+        <div class="row gy-2 overflow-hidden">
+            <div class="col-12">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Username">
+                    <label for="name" class="form-label font-weight-normal">Username</label>
+                    @error('name')
+                        {{ $message }}
+                    @enderror
                 </div>
             </div>
-        </div>
-    </div>
-</section>
+            <div class="col-12">
+                <div class="form-floating mb-3">
+                    <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+                    <label for="email" class="form-label font-weight-normal">Email</label>
+                    @error('email')
+                        {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                    <label for="password" class="form-label font-weight-normal">Password</label>
+                    @error('password')
+                        {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-floating mb-3">
+                    <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
+                        placeholder="Confirm Password">
+                    <label for="password_confirmation" class="form-label font-weight-normal">Confirm Password</label>
+                </div>
+            </div>
 
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
-</body>
-</html>
+            <div class="col-12">
+                <div class="d-grid my-3">
+                    <button class="btn btn-primary btn-lg" type="submit">Register</button>
+                </div>
+            </div>
+            <div class="col-12">
+                <p class="m-0 text-secondary text-center">Already have an account? <a href="{{ route('login.store')}}"
+                        class="link-primary text-decoration-none">Login</a></p>
+            </div>
+        </div>
+    </form>
+@endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,58 +10,22 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
-<body>
 
-<section class="bg-light py-3 py-md-5">
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
-          <div class="card border border-light-subtle rounded-3 shadow-sm">
-            <div class="card-body p-3 p-md-4 p-xl-5">
-              <div class="text-center mb-3">
-                <a href="#!">
-                  <p>Shafura Digital Indonesia</p>
-                </a>
-              </div>
-              <h2 class="fs-6 fw-normal text-center text-secondary mb-4">Enter your details to Login</h2>
-              <form action="#!">
-                <div class="row gy-2 overflow-hidden">
+<body class="bg-light">
 
-                  <div class="col-12">
-                    <div class="form-floating mb-3">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com" required>
-                      <label for="email" class="form-label">Email</label>
+    <section class="d-flex justify-content-center align-items-center min-vh-100">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4">
+                    <div class="card border border-light-subtle rounded-3 shadow-sm">
+                        <div class="card-body p-3 p-md-4 p-xl-5">
+                            @yield('main')
+                        </div>
                     </div>
-                  </div>
-                  <div class="col-12">
-                    <div class="form-floating mb-3">
-                      <input type="password" class="form-control" name="password" id="password" value="" placeholder="Password" required>
-                      <label for="password" class="form-label">Password</label>
-                    </div>
-                  </div>
-
-                  <div class="col-12">
-                    <div class="d-grid my-3">
-                      <button class="btn btn-primary btn-lg" type="submit">Login</button>
-                    </div>
-                  </div>
-                  <div class="col-12">
-                    <p class="m-0 text-secondary text-center">Not have an account? <a href="#!" class="link-primary text-decoration-none">Register</a></p>
-                  </div>
                 </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  </section>
-
-    <!-- jQuery -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+    </section>
 </body>
+
 </html>
