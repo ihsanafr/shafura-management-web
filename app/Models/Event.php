b97b61fad107ff85ff087df11f8d92e0701537b0
Dean Denaki <sonicmailfaster@gmail.com>
2025-05-08 08:49:51 +0700

    some little changes in model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+//Agenda model (sorry for not renaming)
 class Event extends Model
 {
     use HasFactory;
